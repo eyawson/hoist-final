99db1ff021113b6eef02feb4f6a7a66e5abb9b7f
eyawson <ebow.yawson@gmail.com>
2020-08-20 00:33:22 -0400

    add variables

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "engineer_name" {
+  type    = list(string)
+  default = ["first.last"]
+}
+
+/* variable "git_repos" {
+  type    = list(string)
+  default = ["covidrepo"]
+}
+variable "github_token" {
+  type = string
+}
+variable "github_token" {
+  type = string
+}
+ */
